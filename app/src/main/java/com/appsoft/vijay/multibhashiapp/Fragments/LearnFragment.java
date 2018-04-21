accefff0ee69584e0ad2e0de7fd6a0574aaef70e
vijay-android <vvsingh890@gmail.com>
2018-04-21 20:01:10 +0530

    fetched json data  successfully and updated fragment based on category
    task completed

--- app/src/main/java/com/appsoft/vijay/multibhashiapp/Fragments/LearnFragment.java
+++ app/src/main/java/com/appsoft/vijay/multibhashiapp/Fragments/LearnFragment.java
@@ -1,25 +1,44 @@
 package com.appsoft.vijay.multibhashiapp.Fragments;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.appsoft.vijay.multibhashiapp.Model.Lesson;
 import com.appsoft.vijay.multibhashiapp.R;
+
+import java.io.IOException;
 
 /**
  * Created by Vijay on 21-Apr-18.
  */
 
 public class LearnFragment extends Fragment {
-    TextView tv;
+    TextView tv_Pronountiation;
+    TextView tv_Concept;
+    Button sound_Button;
+    String audioURL;
+
+    MediaPlayer mediaplayer;
+
     public LearnFragment() {
                 super();
 
+    }
+
+    public static LearnFragment newInstance(Lesson lesson) {
+
+        LearnFragment f = new LearnFragment();
+        Bundle b = new Bundle();
+        b.putSerializable("learn", lesson);
+        f.setArguments(b);
+        return f;
     }
 
     @Nullable
@@ -27,20 +46,49 @@ public class LearnFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_learn, container, false);
 
-       tv = (TextView) v.findViewById(R.id.text_Pronountiation);
+        tv_Pronountiation = (TextView) v.findViewById(R.id.text_Pronountiation);
+        tv_Concept = (TextView) v.findViewById(R.id.learn_concept);
+        sound_Button = (Button) v.findViewById(R.id.button_Play);
+
         Lesson lesson = (Lesson) getArguments().getSerializable("learn");
-        tv.setText(lesson.getAudioUrl());
+
+        tv_Concept.setText(lesson.getConceptName());
+        tv_Pronountiation.setText(lesson.getPronunciation());
+        audioURL = lesson.getAudioUrl();
+        sound_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    mediaplayer = new MediaPlayer();
+                    mediaplayer.setDataSource(audioURL);
+                    mediaplayer.prepare();
+
+
+                } catch (IllegalArgumentException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (SecurityException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IllegalStateException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                mediaplayer.start();
+
+
+            }
+        });
+
+
         return v;
 
     }
-    public static LearnFragment newInstance(Lesson lesson) {
 
-        LearnFragment f = new LearnFragment();
-        Bundle b = new Bundle();
-        b.putSerializable("learn",lesson);
-        f.setArguments(b);
-        return f;
-    }
     @Override public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/appsoft/vijay/multibhashiapp/Fragments/QuestioFragment.java
+++ app/src/main/java/com/appsoft/vijay/multibhashiapp/Fragments/QuestioFragment.java
@@ -1,15 +1,26 @@
 package com.appsoft.vijay.multibhashiapp.Fragments;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.appsoft.vijay.multibhashiapp.Model.Lesson;
 import com.appsoft.vijay.multibhashiapp.R;
+import com.appsoft.vijay.multibhashiapp.Utility.TextMatcher;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by Vijay on 21-Apr-18.
@@ -17,9 +28,26 @@ import com.appsoft.vijay.multibhashiapp.R;
 
 public class QuestioFragment extends Fragment{
 
-    TextView tv;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    TextView tv_Result;
+    TextView tv_Pronountiation;
+    TextView tv_Concept;
+    TextMatcher matcher;
+    private TextView txtSpeechInput;
+    private ImageButton btnSpeak;
+
     public QuestioFragment() {
         super();
+    }
+
+    public static QuestioFragment newInstance(Lesson lesson) {
+
+        QuestioFragment f = new QuestioFragment();
+        Bundle b = new Bundle();
+        b.putSerializable("question", lesson);
+        f.setArguments(b);
+
+        return f;
     }
 
     @Nullable
@@ -27,27 +55,76 @@ public class QuestioFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_question, container, false);
 
-         tv = (TextView) v.findViewById(R.id.tvFragSecond);
+        tv_Pronountiation = (TextView) v.findViewById(R.id.text_Pronountiation);
+        txtSpeechInput = (TextView) v.findViewById(R.id.txtSpeechInput);
+        tv_Concept = (TextView) v.findViewById(R.id.learn_concept);
+        btnSpeak = (ImageButton) v.findViewById(R.id.btnSpeak);
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
         Lesson lesson = (Lesson) getArguments().getSerializable("question");
-        tv.setText(lesson.getAudioUrl());
+        tv_Concept.setText(lesson.getConceptName());
+        tv_Pronountiation.setText(lesson.getPronunciation());
+
         return v;
 
     }
 
-
-    public static QuestioFragment newInstance(Lesson lesson) {
-
-        QuestioFragment f = new QuestioFragment();
-        Bundle b = new Bundle();
-        b.putSerializable("question",lesson);
-        f.setArguments(b);
-
-        return f;
-    }
     @Override public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        matcher = new TextMatcher();
 
     }
+
+    /**
+     * Showing google speech input dialog
+     */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getActivity().getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Receiving speech input
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
+                    int perc = TextMatcher.pecentageOfTextMatch(tv_Pronountiation.getText().toString(), result.get(0));
+
+
+                    Toast.makeText(getActivity().getApplicationContext(),
+                            Integer.toString(perc) + "\t" + "%" + "\t" + "Matched",
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+
+        }
+    }
+
 }
